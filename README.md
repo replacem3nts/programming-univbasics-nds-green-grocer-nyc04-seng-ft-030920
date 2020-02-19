e8b4a911e19d89ddcf2abbc9df90264027941497
Donny L <donny.landis@gmail.com>
2020-02-19 18:57:53 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Green Grocer
+gr# Green Grocer
 
 ## Learning Goals
 
--- grocer.rb
+++ grocer.rb
@@ -1,36 +1,107 @@
+require 'pp'
+
+# this method searches a collection (AoH) and 
+# if name matches value of ":item" returns the hash
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+    i = 0
+    while i < collection.length do
+      item_hash = collection[i]
+      item_hash[:item] == name ? (return item_hash) : nil
+      i += 1
+    end
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  new_cart = [{:item => nil}]
+  i = 0
+  while i < cart.length do
+    cart_hash = cart[i]
+    if !find_item_by_name_in_collection(cart_hash[:item], new_cart)
+      new_cart << cart_hash
+      new_cart.last[:count] = 1
+    else
+      n = 0
+      while n < new_cart.length do
+        if new_cart[n][:item] == cart_hash[:item]
+          new_cart[n][:count] += 1
+        end
+        n += 1
+      end
+    end
+    i += 1
+  end
+  new_cart.shift
+  new_cart
+end
+
+
+def new_coupon(coupon_hash, cart_hash)
+  coupon = {:item => "#{cart_hash[:item]} W/COUPON", :price => (coupon_hash[:cost] / coupon_hash[:num]), :clearance => cart_hash[:clearance], :count => (cart_hash[:count] - (cart_hash[:count] % coupon_hash[:num]))}
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i = 0
+  while i < coupons.count do
+    coupon_hash = coupons[i]
+    n = 0
+    while n < cart.length do
+      cart_hash = cart[n]
+      if (coupon_hash[:item] == cart_hash[:item]) && (cart_hash[:count] >= coupon_hash[:num])
+          cart << new_coupon(coupon_hash, cart_hash)
+          cart_hash[:count] = (cart_hash[:count] - cart.last[:count])
+      end
+      n += 1
+    end
+  i += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i = 0
+  discount = 0.2
+  while i < cart.length do
+    cart_hash = cart[i]
+    if cart_hash[:clearance] == true
+      cart_hash[:price] = (cart_hash[:price] * (1 - discount)).round(1)
+    end
+    i += 1
+  end
+  cart
+end
+
+def run_total(cart)
+  subtotal = 0
+  i = 0
+  while i < cart.length do
+    item_price = cart[i][:price]
+    item_no = cart[i][:count]
+    subtotal += (item_price * item_no)
+    i += 1
+  end
+  subtotal
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  subtotal = 0
+  cons_cart = consolidate_cart(cart)
+  subtotal = run_total(cons_cart)
+  
+  if coupons.length != 0
+    coup_cart = apply_coupons(cons_cart, coupons)
+    subtotal = run_total(coup_cart)
+  else
+    coup_cart = cons_cart
+  end
+
+  clear_cart = apply_clearance(coup_cart)  
+  big_spend_disc = 0.1
+  if run_total(clear_cart) > 100
+    subtotal = (run_total(clear_cart) * (1 - big_spend_disc)).round(1)
+  else
+    subtotal = run_total(clear_cart)
+  end
+  
+  subtotal
 end
